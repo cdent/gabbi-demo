e5f17c8479abc0b5c84614d92c19ef9d8b470aca
Chris Dent <chris.dent@gmail.com>
2015-05-08 21:37:26 +0100

    Demonstrate fixtures
    A fixture in gabbi is per test file. start is called before the
    first test and stop is called either after the last test or after
    something blows up.
    
    This fixture is is creating a list of objects in a container called
    'fixtured' so we can do some later experimenting with that data.
    
    The new test confirms that the right data is created. Note the use
    of '$.objects' with a value of a YAML list.

--- gdemo/tests/test_gabbi.py
+++ gdemo/tests/test_gabbi.py
@@ -1,16 +1,40 @@
 """A test module to load gabbi-demo tests."""
 
 import os
+import sys
 
 from gabbi import driver
+from gabbi import fixture
 
 from gdemo import load_app
+from gdemo import DATA_STORE
 
 TESTS_DIR = 'gabbits'
+NAMES = [u'alpha', u'beta', u'gamma', u'delta', u'epsilon', u'zeta']
+
+
+class SampleDataFixture(fixture.GabbiFixture):
+
+    def start_fixture(self):
+        """Create some sample data"""
+        objects = {}
+        for name in NAMES:
+            object_data = {'type': 'text/plain',
+                           'body': name.encode('UTF-8')}
+            objects[name] = object_data
+        DATA_STORE['fixtured'] = {'owner': 'this',
+                                  'objects': objects}
+
+    def stop_fixture(self):
+        try:
+            del DATA_STORE['fixtured']
+        except KeyError:
+            pass
 
 
 def load_tests(loader, tests, pattern):
     """Provide a TestSuite to the discovery process."""
     test_dir = os.path.join(os.path.dirname(__file__), TESTS_DIR)
     return driver.build_tests(test_dir, loader, host=None,
-                              intercept=load_app)
+                              intercept=load_app,
+                              fixture_module=sys.modules[__name__])
