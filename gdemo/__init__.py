59eef3301c38a8da7564fd1dcba9dba6852a4ee7
Chris Dent <chris.dent@gmail.com>
2015-05-08 21:09:00 +0100

    Implement creating and listing objects
    The change makes it possible to POST or PUT an object into a
    container and create a list of those objects in a container.
    
    The main new gabbi feature being demonstrated here is reading body
    data from a file with <@ signal. In this case it is an image of a
    kitten because all demos need kittens.
    
    Note that indexes on JSONPath lists can be - if required.

--- gdemo/__init__.py
+++ gdemo/__init__.py
@@ -30,7 +30,7 @@ def create_container(environ, start_response):
 
     DATA_STORE[container_id] = {
         'owner': data['owner'],
-        'objects': []
+        'objects': {}
     }
 
     location = request.relative_url(container_id)
@@ -71,11 +71,69 @@ def get_container(environ, start_response):
     return [data.encode('UTF-8')]
 
 
+def get_object(environ, start_response):
+    container_id = _get_route_value(environ, 'container_id')
+    object_id = _get_route_value(environ, 'object_id')
+
+    try:
+        object = DATA_STORE[container_id]['objects'][object_id]
+    except KeyError:
+        start_response('404 Not Found', [])
+        return []
+
+    start_response('200 OK',
+                   [('Content-Type', object['type'])])
+    return [object['body']]
+
+
 def get_root(environ, start_response):
     start_response('200 OK',
                    [('Content-Type', 'text/html; charset=UTF-8')])
     frontpage = open(FRONT_PAGE, 'rb')
     return frontpage
+
+
+def list_objects(environ, start_response):
+    container_id = _get_route_value(environ, 'container_id')
+
+    try:
+        objects = DATA_STORE[container_id]['objects']
+    except KeyError:
+        start_response('404 Not Found', [])
+        return []
+
+    objects = sorted(objects.keys())
+    # container-ize the objects to avoid top level json list
+    data = json.dumps({'objects': objects})
+
+    start_response('200 OK',
+                   [('Content-Type', 'application/json')])
+
+    return [data.encode('UTF-8')]
+
+
+def create_object(environ, start_response):
+    container_id = _get_route_value(environ, 'container_id')
+    object_id = str(uuid.uuid4())
+    request = webob.Request(environ)
+
+    object_data = {
+        'type': request.content_type,
+        'body': request.body  # no encoding handling
+    }
+
+    try:
+        DATA_STORE[container_id]['objects'][object_id] = object_data
+    except KeyError:
+        start_response('404 Not Found', [])
+        return []
+
+    location = request.relative_url('objects/%s' % object_id)
+
+    start_response('201 Created',
+                   [('Location', location)])
+
+    return []
 
 
 def update_container(environ, start_response):
@@ -90,12 +148,41 @@ def update_container(environ, start_response):
         response_code = '201 Created'
         DATA_STORE[container_id] = {
             'owner': data['owner'],
-            'objects': []
+            'objects': {}
         }
 
-    location = request.relative_url(container_id)
+    location = request.url
 
     start_response(response_code,
+                   [('Location', location)])
+
+    return []
+
+
+def update_object(environ, start_response):
+    container_id = _get_route_value(environ, 'container_id')
+    object_id = _get_route_value(environ, 'object_id')
+    request = webob.Request(environ)
+
+    object_data = {
+        'type': request.content_type,
+        'body': request.body  # no encoding handling
+    }
+
+    if object_id in DATA_STORE[container_id]['objects']:
+        status = '204 No Content'
+    else:
+        status = '201 Created'
+
+    try:
+        DATA_STORE[container_id]['objects'][object_id] = object_data
+    except KeyError:
+        start_response('404 Not Found', [])
+        return []
+
+    location = request.url
+
+    start_response(status,
                    [('Location', location)])
 
     return []
