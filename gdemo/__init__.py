fdb628a265be887fb5cf9a53fbb41ded6e5bab68
Chris Dent <chris.dent@gmail.com>
2015-05-08 18:38:10 +0100

    Implement creating a container
    Note that there are two tests in the new container.yaml file, but
    one of them will fail because GETting a container is not yet
    implemented.
    
    Note too that there are no plans to make it possible to list
    containers. This is by design.
    
    There are four new pieces of gabbi in this commit:
    
    * When POSTing with gabbi, you must provide a content-type and some data
      in some form. If the content-type is JSON you can provide a structured
      piece of YAML and it will become JSON in processing. Other ways to
      provide data will be investigated later.
    
    * When evaluating a response header if you enclose the value in '/'
      it will be treated as a regular expression. This allows partial
      matches on header values for which we do not know the exact value.
      In this case it is necessary because the URI in the location header
      includes a uuid.
    
    * The second test (the one that fails) includes the magic variable
      $LOCATION. When the tests are run this will be replaced by the
      value of the location header in the previous response.
    
    * The second test also provides the first appearance of
      response_json_paths. If a response is JSON we can evaluate it with
      JSONPath.

--- gdemo/__init__.py
+++ gdemo/__init__.py
@@ -4,16 +4,36 @@ A simple web app to demonstrate storage and retrieval.
 This is not meant to demonstrate WSGI app best practice!
 """
 
+import json
 import pkg_resources
+import uuid
+
+import webob
 
 from gdemo import selector
 
 URLS_MAP = pkg_resources.resource_filename('gdemo', 'urls.map')
 FRONT_PAGE = pkg_resources.resource_filename('gdemo', 'frontpage.html')
 
+DATA_STORE = {}
 
 def load_app():
     return selector.Selector(mapfile=URLS_MAP)
+
+
+def create_container(environ, start_response):
+    request = webob.Request(environ)
+    data = json.loads(request.body.decode('UTF-8'))
+    container_id = str(uuid.uuid4())
+
+    DATA_STORE[container_id] = data
+
+    location = request.relative_url(container_id)
+
+    start_response('201 Created',
+                   [('Location', location)])
+
+    return []
 
 
 def get_root(environ, start_response):
